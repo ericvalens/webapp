95206dc69232525e3fd3091be49820c2c71306a9
ericvalens <eric.e.valens@gmail.com>
2022-06-05 20:51:52 -0700

    add metadata

--- components/controller.js
+++ components/controller.js
@@ -1,23 +1,43 @@
+import { useState } from "react";
 import { Joystick } from "react-joystick-component";
 
 export default function Controller() {
+  const [state, setState] = useState({});
+
   const onMove = (stick) => {
     const { type, x, y, direction, distance } = stick;
-    console.log(`${type}, ${x}, ${y}, ${direction}, ${distance}`);
+    setState({ type, x, y, direction, distance });
   };
 
   const onStop = (stick) => {
     const { type } = stick;
-    console.log(`${type}`);
+    setState({ type });
   };
 
   return (
-    <div className="bg-white max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-screen">
-      <div className="max-w-3xl mx-auto flex justify-center h-full">
+    <div className="mx-auto h-screen max-w-7xl bg-white px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto flex h-full max-w-3xl justify-center">
         <div className="flex flex-col">
-          <br className="flex-none" />
-          <div className="flex flex-col grow justify-center pt-96">
-            <div className="pt-24">
+          <div className="mt-12 flex-none text-center text-2xl font-extrabold text-gray-900 sm:text-3xl sm:tracking-tight lg:text-4xl">
+            <ul className="h-60 list-none">
+              <li className="uppercase">Type: {state.type}</li>
+              {state.distance && (
+                <li className="pt-4 uppercase">
+                  Distance: {parseInt(state.distance)}
+                </li>
+              )}
+              {state.direction && (
+                <li className="pt-4 uppercase">Direction: {state.direction}</li>
+              )}
+              {state.x && state.y && (
+                <li className="pt-4 uppercase">
+                  Coordinates: x: {parseInt(state.x)}, y: {parseInt(state.y)}
+                </li>
+              )}
+            </ul>
+          </div>
+          <div className="flex grow flex-col justify-center">
+            <div className="flex justify-center">
               <Joystick
                 size={100}
                 throttle={500}
